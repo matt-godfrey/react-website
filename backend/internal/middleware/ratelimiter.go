ec6c030b761fbcd8f40b9c67529711dafbad6bb0
Matt Godfrey <godfrey.m1992@gmail.com>
2026-06-21 14:40:21 -0400

    fix: reset timestamp array in rate limiter when all previous requests are outside of window

--- backend/internal/middleware/ratelimiter.go
+++ backend/internal/middleware/ratelimiter.go
@@ -6,7 +6,7 @@ import (
 )
 
 type RateLimiter interface {
-	Allow(ip string) (bool, time.Duration)
+	Allow(ip string) (bool, float64)
 }
 
 type Config struct {
@@ -30,12 +30,11 @@ func NewSlidingWindowRateLimiter(limit int, window time.Duration) *SlidingWindow
 	}
 }
 
-func (rl *SlidingWindowRateLimiter) Allow(ip string) (bool, time.Duration) {
+func (rl *SlidingWindowRateLimiter) Allow(ip string) (bool, float64) {
 	rl.Lock()
 	defer rl.Unlock()
 
 	rl.clients[ip] = append(rl.clients[ip], time.Now())
-
 	// need to remove timestamps outside of window
 	for i, t := range rl.clients[ip] {
 		if time.Since(t) > rl.window {
@@ -48,7 +47,12 @@ func (rl *SlidingWindowRateLimiter) Allow(ip string) (bool, time.Duration) {
 	if len(rl.clients[ip]) > rl.limit {
 		// this is the time when the oldest request in the window will expire
 		retryAfter := rl.window - time.Since(rl.clients[ip][0])
-		return false, retryAfter
+		if retryAfter < 0 {
+			// reset array
+			rl.clients[ip] = append([]time.Time{}, time.Now())
+			return true, 0
+		}
+		return false, retryAfter.Seconds()
 	}
 
 	return true, 0
