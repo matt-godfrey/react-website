5423f234bcd19ab955e5a1bd92a5a0f9415160cb
Matt Godfrey <godfrey.m1992@gmail.com>
2026-07-01 14:52:06 -0400

    feat: add quote skeleton to display when loading data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import "./App.css";
 import Quote from "./components/Quote";
 
--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,14 +1,18 @@
 import { randomQuoteQuery } from "@/api/quoteQuery";
 import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { Card } from "./ui/card";
+import QuoteSkeleton from "./skeletons/quoteSkeleton";
 
 interface QuoteProps {}
 
 export default function Quote({}: QuoteProps) {
-  const { data } = useQuery(randomQuoteQuery());
+  const { data, isLoading } = useQuery(randomQuoteQuery());
   const quote = data?.q.trim();
   const author = data?.a;
-  return (
+  return isLoading ? (
+    // <p>Loading...</p>
+    <QuoteSkeleton />
+  ) : (
     <Card>
       <div>
         <p className="font-mono p-4">&ldquo;{quote}&rdquo;</p>
